01d9d1de310b76b4edbf6a347468eb1d50feede6
ANAIS CAVAILLE <anais.cavaille93@gmail.com>
2023-11-15 14:24:21 +0100

    SQLite

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,88 +1,97 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import sqlite from "sqlite3";
+import { Ad } from "../types";
+
+const db = new sqlite.Database("the_good_corner.sqlite");
 
 const app = express();
+const port = 4000;
 
 app.use(express.json());
 
-const port = 4000;
-
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.get("/ad", (req, res) => {
-  res.send(ads);
-});
-
-app.get("/ad/:id", (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  res.send(ads.find((ad) => ad.id === id));
-});
-
-app.post("/ad", (req, res) => {
-  ads.push(req.body);
-  res.send("Request received, check the backend terminal");
-});
-
-app.delete("/ad/:id", (req, res) => {
-  console.log(`deleting ad #${req.params.id}`);
-
-  ads = ads.filter((ad) => req.params.id !== ad.id.toString());
-
-  // Autre méthodes :
-
-  // for (let i = 0; i < ads.length; i++) {
-  //   if (req.params.id === ads[i].id.toString()) {
-  //     ads.splice(i, 1);
-  //   }
-  // }
-
-  // ads.splice(
-  //   ads.findIndex((ad) => ad.id.toString() === req.params.id),
-  //   1
-  // );
-
-  res.sendStatus(204);
-});
-
-app.patch("/ad/:id", (req, res) => {
-  ads = ads.map((ad) => {
-    if (ad.id.toString() === req.params.id) {
-      return { ...ad, ...req.body };
-    }
-    return ad;
+app.get("/ads", (req: Request, res: Response) => {
+  db.all("SELECT * FROM ad", (err, rows: Ad[]) => {
+    if (!err) return res.send(rows);
+    console.log(err);
+    res.sendStatus(500);
   });
+});
 
-  res.send(ads.find((ad) => ad.id.toString() === req.params.id));
+app.post("/ads", (req: Request, res: Response) => {
+  const newAd: Ad = {
+    ...req.body,
+    createdAt: new Date().toISOString(),
+  };
+
+  db.run(
+    "INSERT INTO ad (title, owner, description, price, picture, location, createdAt) VALUES ($title, $owner, $description, $price, $picture, $location, $createdAt)",
+    {
+      $title: newAd.title,
+      $owner: newAd.owner,
+      $description: newAd.description,
+      $price: newAd.price,
+      $picture: newAd.picture,
+      $location: newAd.location,
+      $createdAt: newAd.createdAt,
+    },
+    function (this: any, err: any) {
+      if (!err) return res.send({ ...newAd, id: this.lastID });
+      console.log(err);
+      res.sendStatus(500);
+    }
+  );
+});
+
+app.delete("/ads/:id", (req: Request, res: Response) => {
+  db.get("SELECT * FROM ad WHERE id = ?", [req.params.id], (err, row) => {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    if (!row) return res.sendStatus(404);
+    db.run("DELETE FROM ad WHERE id = ?", [req.params.id], (err: any) => {
+      if (!err) return res.sendStatus(204);
+      console.log(err);
+      res.sendStatus(500);
+    });
+  });
+});
+
+app.patch("/ads/:id", (req: Request, res: Response) => {
+  db.get("SELECT * FROM ad WHERE id = ?", [req.params.id], (err, row) => {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    if (!row) return res.sendStatus(404);
+
+    // creates a string with this shape : "title = $title, description = $description, ..."
+    const setProps = Object.keys(req.body)
+      .reduce<string[]>((acc, prop) => [...acc, `${prop} = $${prop}`], [])
+      .join(", ");
+
+    // creates an object with this shape : {$title: "title sent by client", "$description: " description sent by client", ...}
+    const propsToUpdate = Object.keys(req.body).reduce(
+      (acc, prop) => ({ ...acc, [`$${prop}`]: req.body[prop] }),
+      {}
+    );
+
+    db.run(
+      `UPDATE ad SET ${setProps} WHERE id = $id`,
+      { ...propsToUpdate, $id: req.params.id },
+      (err: any) => {
+        if (!err) return res.send({ ...row, ...req.body });
+        console.log(err);
+        res.sendStatus(500);
+      }
+    );
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
-
-let ads = [
-  {
-    id: 1,
-    title: "Bike to sell",
-    description:
-      "My bike is blue, working fine. I'm selling it because I've got a new one",
-    owner: "bike.seller@gmail.com",
-    price: 100,
-    picture:
-      "https://images.lecho.be/view?iid=dc:113129565&context=ONLINE&ratio=16/9&width=640&u=1508242455000",
-    location: "Paris",
-    createdAt: "2023-09-05T10:13:14.755Z",
-  },
-  {
-    id: 2,
-    title: "Car to sell",
-    description:
-      "My car is blue, working fine. I'm selling it because I've got a new one",
-    owner: "car.seller@gmail.com",
-    price: 10000,
-    picture:
-      "https://www.automobile-magazine.fr/asset/cms/34973/config/28294/apres-plusieurs-prototypes-la-bollore-bluecar-a-fini-par-devoiler-sa-version-definitive.jpg",
-    location: "Paris",
-    createdAt: "2023-10-05T10:14:15.922Z",
-  },
-];
