4a3b522dd9ec4be180c28be06ada4fb058799c9a
ANAIS CAVAILLE <anais.cavaille93@gmail.com>
2023-11-20 17:03:37 +0100

    ajout front

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import RecentAds from "../components/RecentAds";
+
+export default function Home() {
+  return (
+    <>
+      <main className="main-content">
+        <Header />
+        <RecentAds />
+      </main>
+    </>
+  );
+}
